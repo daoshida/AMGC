83bb5147d458e3d666dc5e4f9839aff511fd3b94
daoshida <19861602215@163.com>
2024-11-30 20:35:06 +0800

    Update AMGC_ResNet18.py

--- AMGC_ResNet18.py
+++ AMGC_ResNet18.py
@@ -85,20 +85,12 @@ class Attention(nn.Module):
         self.scale = torch.sqrt(torch.tensor(self.head_dim,dtype=torch.float))
 
     def forward(self, query, key, value, mask=None):
-        # K: [64,10,300], batch_size 为 64，有 12 个词，每个词的 Query 向量是 300 维
-        # V: [64,10,300], batch_size 为 64，有 10 个词，每个词的 Query 向量是 300 维
-        # Q: [64,12,300], batch_size 为 64，有 10 个词，每个词的 Query 向量是 300 维
+        
         bsz = query.shape[0]
         Q = self.w_q(query)
         K = self.w_k(key)
         V = self.w_v(value)
-        # 这里把 K Q V 矩阵拆分为多组注意力，变成了一个 4 维的矩阵
-        # 最后一维就是是用 self.hid_dim // self.n_heads 来得到的，表示每组注意力的向量长度, 每个 head 的向量长度是：300/6=50
-        # 64 表示 batch size，6 表示有 6组注意力，10 表示有 10 词，50 表示每组注意力的词的向量长度
-        # K: [64,10,300] 拆分多组注意力 -> [64,10,6,50] 转置得到 -> [64,6,10,50]
-        # V: [64,10,300] 拆分多组注意力 -> [64,10,6,50] 转置得到 -> [64,6,10,50]
-        # Q: [64,12,300] 拆分多组注意力 -> [64,12,6,50] 转置得到 -> [64,6,12,50]
-        # 转置是为了把注意力的数量 6 放到前面，把 10 和 50 放到后面，方便下面计算
+        
         Q = Q.view(bsz, -1, self.n_heads, self.hid_dim //
                    self.n_heads).permute(0, 2, 1, 3)
         K = K.view(bsz, -1, self.n_heads, self.hid_dim //
@@ -111,26 +103,17 @@ class Attention(nn.Module):
         # attention：[64,6,12,10]
         attention = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
 
-        # 把 mask 不为空，那么就把 mask 为 0 的位置的 attention 分数设置为 -1e10
         if mask is not None:
             attention = attention.masked_fill(mask == 0, -1e10)
 
-        # 第 2 步：计算上一步结果的 softmax，再经过 dropout，得到 attention。
-        # 注意，这里是对最后一维做 softmax，也就是在输入序列的维度做 softmax
-        # attention: [64,6,12,10]
         attention = self.do(torch.softmax(attention, dim=-1))
 
-        # 第三步，attention结果与V相乘，得到多头注意力的结果
         # [64,6,12,10] * [64,6,10,50] = [64,6,12,50]
         # x: [64,6,12,50]
         x = torch.matmul(attention, V)
 
-        # 因为 query 有 12 个词，所以把 12 放到前面，把 5 和 60 放到后面，方便下面拼接多组的结果
-        # x: [64,6,12,50] 转置-> [64,12,6,50]
         x = x.permute(0, 2, 1, 3).contiguous()
-        # 这里的矩阵转换就是：把多组注意力的结果拼接起来
-        # 最终结果就是 [64,12,300]
-        # x: [64,12,6,50] -> [64,12,300]
+       
         x = x.view(bsz, -1, self.n_heads * (self.hid_dim // self.n_heads))
         x = self.fc(x)
         return x
